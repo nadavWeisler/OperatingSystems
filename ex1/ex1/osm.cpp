9fa9300da908d5ca292822461e2892325ef4b5fc
orianelouzoun <oriane.louzoun@mail.huji.ac.il>
2021-04-13 12:02:15 +0300

    Oriane : sumbited
    replaced nullptr with NULL

--- ex1/ex1/osm.cpp
+++ ex1/ex1/osm.cpp
@@ -9,30 +9,18 @@
 /**
  * An empty function
  */
-void empty_func() {
-
-}
-
-/**
- * get the nano second representation
- * @param t
- * @return
- */
+void empty_func() {}
 long get_nano(struct timeval t) {
     return ((t.tv_sec * 1000000 + t.tv_usec) * 1000);
 }
 
-/* Time measurement function for a simple arithmetic operation.
-   returns time in nano-seconds upon success,
-   and -1 upon failure.
-   */
 double osm_operation_time(unsigned int iterations) {
-    if(iterations == 0) {
+    if (iterations == 0) {
         return 0;
     }
     timeval start_time;
     int a = 0, b = 1;
-    gettimeofday(&start_time, nullptr);
+    gettimeofday(&start_time, NULL);
     for (int i = 0; i < (int) iterations; i++) {
         a += b;
         a += b;
@@ -47,9 +35,8 @@ double osm_operation_time(unsigned int iterations) {
         a += b;
     }
     timeval end_time;
-    gettimeofday(&end_time, nullptr);
+    gettimeofday(&end_time, NULL);
     return (double) ((get_nano(end_time)) - get_nano(start_time)) / (iterations * 11);
-
 }
 
 
@@ -58,11 +45,11 @@ double osm_operation_time(unsigned int iterations) {
    and -1 upon failure.
    */
 double osm_function_time(unsigned int iterations) {
-    if(iterations == 0) {
+    if (iterations == 0) {
         return 0;
     }
     timeval start_time;
-    gettimeofday(&start_time, nullptr);
+    gettimeofday(&start_time, NULL);
     for (int i = 0; i < (int) iterations; i++) {
         empty_func();
         empty_func();
@@ -77,43 +64,32 @@ double osm_function_time(unsigned int iterations) {
         empty_func();
     }
     timeval end_time;
-    gettimeofday(&end_time, nullptr);
+    gettimeofday(&end_time, NULL);
     return (double) ((get_nano(end_time)) - get_nano(start_time)) / (iterations * 11);
 }
 
-
-/* Time measurement function for an empty trap into the operating system.
-   returns time in nano-seconds upon success,
-   and -1 upon failure.
-   */
 double osm_syscall_time(unsigned int iterations) {
-    if(iterations == 0) {
+    if (iterations == 0) {
         return 0;
     }
     timeval start_time;
-    gettimeofday(&start_time, nullptr);
+    gettimeofday(&start_time, NULL);
     for (int i = 0; i < (int) iterations; i++) {
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
-        OSM_nullptrSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
+        OSM_NULLSYSCALL;
     }
     timeval end_time;
-    gettimeofday(&end_time, nullptr);
+    gettimeofday(&end_time, NULL);
     return (double) ((get_nano(end_time)) - get_nano(start_time)) / (iterations * 11);
 }
 
 
-/**int main() {
-    int iterations = 100000;
-    std::cout << "System calls time: " << osm_syscall_time(iterations)
-    << std::endl << "Functions time: " << osm_function_time(iterations)
-              << std::endl << "Operations time: " << osm_operation_time(iterations) << std::endl;
-}/
