01de644219f30db29c29ca3ef95e1a40d9250360
orianelouzoun <oriane.louzoun@mail.huji.ac.il>
2021-05-04 14:25:24 +0300

    started-Oriane

--- ex2/uthreads.cpp
+++ ex2/uthreads.cpp
@@ -2,3 +2,25 @@
 // Created by Weisl on 4/30/2021.
 //
 
+#include "uthreads.h"
+
+int uthread_init(int quantum_usecs)
+{
+// בעצם מאפסת את הטבלה של הטרדים?
+
+}
+
+int uthread_spawn(void (*f)(void))
+{
+
+}
+
+int uthread_terminate(int tid)
+{
+
+}
+
+int uthread_block(int tid)
+{
+
+}
